a83ec4b48f03ec566bf42ce76ae26bd8222966fd
Ayanami_Asuka <fyb0118@gmail.com>
2017-11-14 21:16:12 +0800

    添加项目文件。

---
+++ CardLib2/Rank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardLib2
+{
+    public enum Rank
+    {
+        Ace,
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
